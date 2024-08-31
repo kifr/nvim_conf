0e13e1be16d74f0b390ede79d706aa8ecb3c40e2
木原健太 <kifr@charcoal.local>
2024-08-31 13:56:40 +0900

    隠しファイル、ディレクトリをデフォルトで表示するように変更

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+
+  -- @see https://github.com/nvim-neo-tree/neo-tree.nvim/blob/main/lua/neo-tree/defaults.lua
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
+    },
+  },
+}
