ea7942b95ba9145ed76d3029d2af0b7b6d402394
木原健太 <kifr@charcoal.local>
2024-08-31 14:05:18 +0900

    colorscheme.lua の追加と catppuccin の設定

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    -- @see https://github.com/catppuccin/nvim/blob/main/README.md#configuration
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
