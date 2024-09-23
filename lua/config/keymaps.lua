ff7bb7c4bc4de1ccbaea243668acd371767d3113
木原健太 <kifr@charcoal.local>
2024-09-23 12:17:44 +0900

    インデント調整の keymap を追加

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local key_mappings = {
+  -- indentation
+  { "n", "<Tab>", ">>", { noremap = true, silent = true } },
+  { "n", "<S-Tab>", "<<", { noremap = true, silent = true } },
+  { "v", "<Tab>", ">gv", { noremap = true, silent = true } },
+  { "v", "<S-Tab>", "<gv", { noremap = true, silent = true } },
+}
+
+for _, map in ipairs(key_mappings) do
+  vim.api.nvim_set_keymap(map[1], map[2], map[3], map[4])
+end
+
 -- diagnostic を浮動ウィンドウで表示
 vim.keymap.set("n", "<S-C-k>", function()
   vim.diagnostic.open_float(nil, {
