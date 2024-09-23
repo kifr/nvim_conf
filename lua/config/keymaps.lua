e995ff8cc519e14bac0febd054733559986d18e0
木原健太 <kifr@charcoal.local>
2024-09-23 09:11:14 +0900

    diagnostic の表示調整

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- diagnostic を浮動ウィンドウで表示
+vim.keymap.set("n", "<S-C-k>", function()
+  vim.diagnostic.open_float(nil, {
+    focusable = true,
+    format = function(diagnostic)
+      return string.format("%s", diagnostic.message)
+    end,
+    suffix = function(diagnostic)
+      return string.format("[%s: %s]", diagnostic.source, diagnostic.code)
+    end,
+    border = "rounded",
+    width = 80,
+    max_width = 100,
+  })
+end)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- インラインで表示される diagnostic を無効化する
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+})
