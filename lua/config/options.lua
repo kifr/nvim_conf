a3cde69120267624a545165ff87a348b2dad08fc
木原健太 <kifr@charcoal.local>
2024-09-23 09:12:06 +0900

    swap ファイルを利用しないように変更

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.swapfile = false
+
 -- インラインで表示される diagnostic を無効化する
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   virtual_text = false,
